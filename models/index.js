0e2f9f33a937bb95da39acba4dec9b2abac770a0
manc1n1 <mancinij1111@gmail.com>
2023-11-03 15:02:50 -0400

    Update sequelize associations

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
 	foreignKey: 'category_id',
+	onDelete: 'CASCADE',
 });
 
 // Categories have many Products
@@ -16,10 +17,16 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+	through: ProductTag,
+	foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+	through: ProductTag,
+	foreignKey: 'tag_id',
+});
 
 module.exports = {
 	Product,
